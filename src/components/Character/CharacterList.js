29bb2769099d2c9fc529a63cda5bb58bee2b0321
Tomas Granda <tomasagustin.granda@mercadolibre.com>
2020-10-20 13:39:16 -0300

    asdf

--- src/components/Character/CharacterList.js
+++ src/components/Character/CharacterList.js
@@ -5,22 +5,25 @@ import { Col } from "react-bootstrap";
 import CharacterCard from "./CharacterCard";
 
 const CharacterList = props => {
-  const characters = props.characters.map((character, i) => {
-    return (
-      <Col xs={3} key={i}>
-        <CharacterCard
-          id={character._id}
-          icon={character.icon}
-          name={character.name}
-          classType={character.classType}
-          life={character.life}
-          defense={character.defense}
-          attack={character.attack}
-          speed={character.speed}
-        />
-      </Col>
-    );
-  });
+  const characters = <div></div>;
+  if(props.characters.length > 0){
+    characters = props.characters.map((character, i) => {
+      return (
+        <Col xs={3} key={i}>
+          <CharacterCard
+            id={character._id}
+            icon={character.icon}
+            name={character.name}
+            classType={character.classType}
+            life={character.life}
+            defense={character.defense}
+            attack={character.attack}
+            speed={character.speed}
+          />
+        </Col>
+      );
+    });
+  }
 
   return characters;
 };
